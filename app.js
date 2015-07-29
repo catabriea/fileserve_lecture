85ebda9596b9fa46299770bc9bd83cde93496f63
Catabriea Muellerleile <cmuellerleile@Catabrieas-MacBook-Air.local>
2015-07-29 14:52:39 -0500

    Second commit

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ var path = require('path');
 
 app.set('port',(process.env.PORT || 5000));
 
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
+    //dropped in * wildcard
+
     var file = req.params[0] || "./public/views/index.html";
-    res.sendFile(path.join(__dirname, "./public/views/index.html"));
+    res.sendFile(path.join(__dirname, "./public/", file));
+    // $$$ big money code
+    // what that does is it sets a default file
 });
 
 app.listen(app.get('port'), function(){
     console.log("Hey! Listening on Port: " + app.get('port'));
 });
 
+//install grunt and grunt-contrib-uglify
